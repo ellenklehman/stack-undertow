4c5014c536f3fc71613196bcd918c3a2d8540cd0
Justin Speers & Ellen Krause Lehman <student@epicodus.com>
2014-09-03 16:43:26 -0700

    WIP: 500 errors involving question view rendering

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @questions = Question.all
     @question = Question.new(question_params)
     if @question.save
       flash[:notice] = "Success!"
